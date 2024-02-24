27b5178d87b41921ba52f123a9666d6b8e31c782
sebastienlestrat <sebastien.le-strat@hotmail.fr>
2024-02-24 19:16:27 +0100

    feat/material service, material interface added

--- recyclascore-api/recyclascore/src/main/java/com/recyclascore/backend/RecyclascoreApplication.java
+++ recyclascore-api/recyclascore/src/main/java/com/recyclascore/backend/RecyclascoreApplication.java
@@ -1,13 +1,13 @@
-package com.recyclascore.api;
+package com.recyclascore.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class RecyclascoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(RecyclascoreApplication.class, args);
 	}
 
 }
--- recyclascore-webfront/recyclascore/src/app/app.component.ts
+++ recyclascore-webfront/recyclascore/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialService } from './services/material.service';
+import { MaterialObject } from './interfaces/material/material-object';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'recyclascore';
+export class AppComponent implements OnInit {
+  title = 'recyclastore';
+  materialObjectList: MaterialObject[] = [];
+  selectedMaterial: MaterialObject | undefined;
+ 
+  constructor(private materialService : MaterialService) {}
+
+  ngOnInit() : void {
+    this.getMaterialList();
+   // this.getMaterial(this.selectedMaterial)
+  }
+
+ public getMaterialList() : void {
+  this.materialService.getMaterialList().subscribe(
+    materialListData => {
+      this.materialObjectList = materialListData;
+    },
+    error => {
+      console.error('Error fetching material list:', error);
+    }
+  );
+  }
+
+  public getMaterial(selectedMaterial : MaterialObject) {
+    this.materialService.getMaterial(selectedMaterial.id).subscribe((material : MaterialObject) => {
+      this.selectedMaterial = material
+    }, error => {
+      console.error('Error fetching material on getMaterial():', error);
+    })
+  }
+  
+
+  onMaterialSelected(event: any) {
+    const selectedMaterialId = event.target.value;
+   // Find the selected material from the list
+   this.selectedMaterial = this.materialObjectList.find(material => material.id === parseInt(selectedMaterialId));
+   if (this.selectedMaterial?.recyclable === true) {
+     'vrai'
+   } else {
+     'false'
+   }
+  }
+
 }
