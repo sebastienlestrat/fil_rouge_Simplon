540236c6747c1c54093d7e6085953bb7fe2225ea
sebastienlestrat <sebastien.le-strat@hotmail.fr>
2024-03-04 22:52:14 +0100

    angular material added - login page created

---
+++ recyclascore-webfront/recyclascore/src/app/login-page/login-page.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit{
+  loginForm!: FormGroup;
+  submitted: boolean = false;
+  hideFormLogin?: boolean;
+
+  constructor(
+    private route: Router,
+    private formBuilder: FormBuilder,
+    public tokenService: TokenService
+  ) {
+       this.hideFormLogin = true;
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // stop here if form is invalid
+    if (!this.loginForm || this.loginForm.invalid) {
+      return;
+    }
+    // get email and password
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    // Call the authentification service
+    this.proceedLogin(email, password);
+  }
+
+  /**
+   * Connexion with email and password
+   */
+  private proceedLogin(email: string, password : string) {
+    this.submitted = true;
+    // call tokenService for connexion
+    this.tokenService.login(email, password).subscribe({
+      next: (accessToken: any) => {
+        console.log('token recovered : %s', accessToken);
+        this.route.navigateByUrl('/app/tabs/tab2');
+      },
+      error: (e: Error) => console.error(e),
+      complete: () => {
+        console.info('logIn succeed');
+        this.submitted = false;
+      },
+    });
+  }
+
+  public navigateToSignUpPage() {
+    this.route.navigateByUrl('/signup');
+  }
+
+}
